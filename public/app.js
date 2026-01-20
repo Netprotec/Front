7786d710a9622cea41cfc9ac012678404151ac2b
Netprotec <libreprogramacion839@gmail.com>
2026-01-20 13:59:28 -0600

    F 1.4

--- public/app.js
+++ public/app.js
@@ -1,28 +1,25 @@
 function enviar() {
   const nombre = document.getElementById('nombre').value;
 
-  // esta linea 
- // fetch('http://localhost:3000/productos', {
-   // method: 'POST',
-    //headers: {
-      //'Content-Type': 'application/json'
-   // },
-    //body: JSON.stringify({ nombre })
-  //})
-fetch('/api/productos', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ nombre })
-});
-
-
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('resultado').textContent =
-      JSON.stringify(data, null, 2);
+  fetch('/api/productos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ nombre })
   })
-  .catch(err => {
-    document.getElementById('resultado').textContent =
-      'Error: ' + err.message;
-  });
+    .then(res => {
+      if (!res.ok) {
+        throw new Error('Error HTTP ' + res.status);
+      }
+      return res.json();
+    })
+    .then(data => {
+      document.getElementById('resultado').textContent =
+        JSON.stringify(data, null, 2);
+    })
+    .catch(err => {
+      document.getElementById('resultado').textContent =
+        'Error: ' + err.message;
+    });
 }
